ade3b42f6600be5d47f37486afe445c83ef606d3
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2022-02-11 12:47:19 +0530

    leetcode problems bothh java and c++

---
+++ sortcolors.cpp
@@ -0,0 +1,30 @@
+
+// solution for the leetcode problem name
+//  sorting colors using three pointers
+//  c++ solution
+
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int mid = 0;
+        while (mid <= hi)
+        {
+            switch (nums[mid])
+            {
+            case 0:
+                swap(nums[lo++], nums[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(nums[mid], nums[hi--]);
+                break;
+            }
+        }
+    }
+};
